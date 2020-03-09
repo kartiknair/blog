ff8c6845d2db366bcdb1a28c7e2b5da2c740c9d5
Kartik Nair <kartiknair1911@gmail.com>
2020-03-09 22:09:17 +0400

    Switched from Gatsby to just regular nodejs

--- gatsby-browser.js
+++
@@ -1,3 +0,0 @@
-import "./src/main.scss"
-import "./src/grotesk.dual.scss"
-import "./src/prism-theme.scss"
---
+++ src/config.js
@@ -0,0 +1,14 @@
+const config = {
+  blogName: "Blog",
+  authorName: "Kartik Nair",
+  authorDescription:
+    "a web developer and designer making lot's of stuff in Dubai",
+  authorTwitter: "https://twitter.com/kartiknair",
+  blogDescription: "Sharing what I learn as a web developer & designer",
+  dev: {
+    postsdir: "./content",
+    outdir: "./public"
+  }
+};
+
+module.exports = config;
